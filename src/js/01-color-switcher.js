45767fe2df2d41d167e6c3294b4ec687851f0f2f
maciejkudej <maciejkudej765@gmail.com>
2023-03-11 21:09:06 +0100

    Add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyElement = document.querySelector('body');
 
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyElement.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
